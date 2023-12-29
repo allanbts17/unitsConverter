f7348d5fbe63df7f5bfbc165c7189bf808f29680
Allan Bennett <allanbts.18music@gmail.com>
2023-12-28 22:34:32 -0500

    feat: first commit

---
+++ app/src/main/java/com/ab/unitsconverter/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ab.unitsconverter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.longitud){
+            Toast.makeText(this, "Click en longitud", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+}
